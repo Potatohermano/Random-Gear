9875f60b168edd839fc874bb6ae181e23b8a72a0
Potatohermano <143749939+Potatohermano@users.noreply.github.com>
2026-04-17 16:28:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+function rollDice1(){ 
+     const numOfDice = document.getElementById("numOfDice").value; 
+     const Helmet = document.getElementById("Helmet"); 
+     const values = [];
+     const images = []; 
+     
+     for(let i = 0; i < numOfDice; i++){ 
+         const value = Math.floor(Math.random() * 27) + 1;
+         values.push(value); 
+         images.push(`<img src="Helmet/${value}.png">`);
+   
+      console.log(values)
+ 
+     Helmet.innerHTML = images.join(''); 
+}}
+
+function rollDice2(){ 
+     const numOfDice = document.getElementById("numOfDice").value; 
+     const Chestplate = document.getElementById("Chestplate"); 
+     const values = [];
+     const images = []; 
+     
+     for(let i = 0; i < numOfDice; i++){ 
+         const value = Math.floor(Math.random() * 27) + 1;
+         values.push(value); 
+         images.push(`<img src="Chestplate/${value}.png">`);
+   
+      console.log(values)
+ 
+     Chestplate.innerHTML = images.join(''); 
+}}
+
+function rollDice3(){ 
+     const numOfDice = document.getElementById("numOfDice").value; 
+     const Feet = document.getElementById("Feet"); 
+     const values = [];
+     const images = []; 
+     
+     for(let i = 0; i < numOfDice; i++){ 
+         const value = Math.floor(Math.random() * 27) + 1;
+         values.push(value); 
+         images.push(`<img src="Boot/${value}.png">`);
+   
+      console.log(values)
+ 
+     Feet.innerHTML = images.join(''); 
+}}
+
+function rollDice4(){ 
+     const numOfDice = document.getElementById("numOfDice").value; 
+     const Cape = document.getElementById("Cape"); 
+     const values = [];
+     const images = []; 
+     
+     for(let i = 0; i < numOfDice; i++){ 
+         const value = Math.floor(Math.random() * 14) + 1;
+         values.push(value); 
+         images.push(`<img src="Cape/${value}.png">`);
+   
+      console.log(values)
+ 
+     Cape.innerHTML = images.join(''); 
+}}
+
+function rollDice5(){ 
+     const numOfDice = document.getElementById("numOfDice").value; 
+     const Weapon = document.getElementById("Weapon"); 
+     const values = [];
+     const images = []; 
+     
+     for(let i = 0; i < numOfDice; i++){ 
+         const value = Math.floor(Math.random() * 27) + 1;
+         values.push(value); 
+         images.push(`<img src="Chestplate/${value}.png">`);
+   
+      console.log(`Weapon ${values}`)
+ 
+     Weapon.innerHTML = images.join(''); 
+}}
